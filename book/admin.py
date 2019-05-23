eba79c4383d5c6bc62c87f6df42952d42a725aee
vyomapatel7 <vyoma.patel07@gmail.com>
2019-05-23 11:39:49 +0200

    Operational error for column author

--- book/admin.py
+++ book/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Profile
+from .models import Book
 
 admin.site.register(Profile)
+admin.site.register(Book)
 
--- book/models.py
+++ book/models.py
@@ -17,4 +17,19 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
     
