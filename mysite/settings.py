e7e21941461b07199b42912710b3f040525061f4
vyomapatel7 <vyoma.patel07@gmail.com>
2019-06-20 14:43:48 +0200

    More Changes to make pythonanywhere work

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'litgproject',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
